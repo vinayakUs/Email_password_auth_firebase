db368fd1d136b37b3addb435216877f902c1bb9d
vinayak <vinayak200054321@gmail.com>
2019-06-29 19:53:08 +0530

    working

--- app/src/main/java/com/example/email_password_auth/create.java
+++ app/src/main/java/com/example/email_password_auth/create.java
@@ -1,5 +1,6 @@
 package com.example.email_password_auth;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,7 @@ public class create extends AppCompatActivity {
     Button create;
     EditText username,password;
     private FirebaseAuth mAuth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +31,14 @@ public class create extends AppCompatActivity {
         username = findViewById(R.id.username_reg_F);
         password = findViewById(R.id.password_reg_F);
         mAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(create.this);
 
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                progressDialog.setMessage("please wait..");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
                 String us=username.getText().toString().trim();
                 String ps=password.getText().toString().trim();
                 register(us,ps);
@@ -41,18 +46,22 @@ public class create extends AppCompatActivity {
         });
 
     }
-    void register(String us,String ps){
+    void register(String us, final String ps){
+
 
         mAuth.createUserWithEmailAndPassword(us,ps)
                 .addOnCompleteListener(create.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.cancel();
                         if(task.isSuccessful()){
+                            progressDialog.dismiss();
                             Intent intent=new Intent(create.this,home.class);
                             intent.addFlags(intent.FLAG_ACTIVITY_CLEAR_TASK|intent.FLAG_ACTIVITY_NEW_TASK);
                             startActivity(intent);
                         }
                         else {
+                            progressDialog.dismiss();
                             Toast.makeText(create.this,"Try again Later",Toast.LENGTH_SHORT).show();
                         }
                     }
