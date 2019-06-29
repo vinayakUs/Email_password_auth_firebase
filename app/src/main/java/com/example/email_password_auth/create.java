ecd5a8b693bf537e5350eaadc7375f5d211389c0
vinayak <vinayak200054321@gmail.com>
2019-06-29 20:17:30 +0530

    working

--- app/src/main/java/com/example/email_password_auth/create.java
+++ app/src/main/java/com/example/email_password_auth/create.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,6 +42,8 @@ public class create extends AppCompatActivity {
                 progressDialog.show();
                 String us=username.getText().toString().trim();
                 String ps=password.getText().toString().trim();
+                if(TextUtils.isEmpty(us)){}
+                if(TextUtils.isEmpty(ps)){}
                 register(us,ps);
             }
         });
