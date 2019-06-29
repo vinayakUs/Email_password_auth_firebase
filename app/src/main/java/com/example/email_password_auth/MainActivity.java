72321198ffa875cedd7f24daafd51045b412b3f2
vinayak <vinayak200054321@gmail.com>
2019-06-29 17:22:34 +0530

    working

--- app/src/main/java/com/example/email_password_auth/MainActivity.java
+++ app/src/main/java/com/example/email_password_auth/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
     Button sign_in,sign_up;
 
@@ -13,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         sign_in=findViewById(R.id.sign_inbtn);
         sign_up=findViewById(R.id.sign_upbtn);
@@ -31,4 +35,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if(currentUser!=null){
+            startActivity(new Intent(MainActivity.this,home.class));
+
+        }
+    }
+
 }
